43b08568e1c332bf3b5eb8cb3e4a3d43d618cef4
TheBugFather <erbeawebird@gmail.com>
2026-08-22 00:19:50 +0700

    Gas

--- server.js
+++ server.js
@@ -15,8 +15,9 @@ app.get('/undangan-resepsi*', (req, res) => {
   res.redirect(301, `https://luxury-undangan-resepsi.vercel.app${subPath}`);
 });
 
-// Serve static assets from project root
+// Serve static assets from project root and public folder
 app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Fallback to index.html for root or SPA routing
 app.get('*', (req, res) => {
